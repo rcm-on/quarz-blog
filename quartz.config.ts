cbaf1db023def6cb26f8d2721c166bf635500857
rcm-on <rcamposmartin@hotmail.com>
2026-05-15 14:00:03 +0200

    update url

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "blog.rcmon.dev",
+    baseUrl: "rcm-on.github.io/quarz-blog",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
